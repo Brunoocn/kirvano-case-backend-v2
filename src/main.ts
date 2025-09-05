f9465645d57461a35b3098d07524efa31db6baae
Bruno Caneo <brunocaneo3@gmail.com>
2025-09-05 01:37:29 -0300

    feat: adicionando modulo do prisma

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const allowedOrigins = process.env.CORS_ALLOWED_ORIGINS
+    ? process.env.CORS_ALLOWED_ORIGINS.split(',')
+    : '';
+  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+  app.enableCors({
+    origin: allowedOrigins,
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('API Kirvano')
+    .setDescription('API para o teste técnico da Kirvano')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  await app.listen(process.env.PORT || 3005);
 }
 bootstrap();
